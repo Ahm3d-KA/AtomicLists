8596f8620acfb2449898cbb6ebb7d1408418147c
Ahm3d-KA <ahmed.kehinde2017@gmail.com>
2024-02-10 19:42:30 +0000

    Create task controller and start create method

---
+++ AtomicLists/Controllers/TaskController.cs
@@ -0,0 +1,29 @@
+﻿using AtomicLists.Data;
+using Microsoft.AspNetCore.Mvc;
+using Task = AtomicLists.Models.Task;
+
+namespace AtomicLists.Controllers;
+
+public class TaskController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public TaskController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    // GET
+    public IActionResult Index()
+    {
+        IEnumerable<Task> objTasksList = _db.UserTasks;
+
+        return View(objTasksList);
+    }
+    public IActionResult Create()
+    {
+        
+
+        return View();
+    }
+}
