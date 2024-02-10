25e81ed32dc3ad5d6e6c54e455d22432f6114169
Ahm3d-KA <ahmed.kehinde2017@gmail.com>
2024-02-10 16:07:02 +0000

    Add initial migration

---
+++ AtomicLists/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtomicLists.Models;
+
+public class Task
+{
+    [Key]
+    public string Id { get; set; }
+    [Required]
+    public string TaskDescription { get; set; }
+    public string Area { get; set; }
+    public bool IsComplete { get; set; }
+    public DateTime DateCreated { get; set; }
+    public string Priority { get; set; }
+    
+}
